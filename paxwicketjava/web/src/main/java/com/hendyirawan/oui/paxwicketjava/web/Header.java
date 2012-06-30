3d789ee2bc6b4bf671e3054cf6de3e6e1747df60
Hendy Irawan <hendy@soluvas.com>
2012-07-01 00:33:48 +0700

    Verryyy nice layout :)

--- paxwicketjava/web/src/main/java/com/hendyirawan/oui/paxwicketjava/web/Header.java
+++ paxwicketjava/web/src/main/java/com/hendyirawan/oui/paxwicketjava/web/Header.java
@@ -1,8 +1,9 @@
 package com.hendyirawan.oui.paxwicketjava.web;
 
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.IModel;
 import org.ops4j.pax.wicket.api.PaxWicketBean;
 
@@ -18,11 +19,19 @@ public class Header extends Panel {
 	
 	public Header() {
 		this(null);
-		RepeatingView rv = new RepeatingView("rv");
-		add(rv);
-		for (Category cat : navigation.getCategories()) {
-			rv.add(new Label(rv.newChildId(), cat.getName()));
-		}
+		setRenderBodyOnly(true);
+		add(new ListView<Category>("navbar", navigation.getCategories()) {
+			@Override
+			protected void populateItem(ListItem<Category> item) {
+				Category category = item.getModelObject();
+				item.add(new ExternalLink("item", "#" + category.getName(), category.getName()));
+			}
+		});
+//		RepeatingView rv = new RepeatingView("rv");
+//		add(rv);
+//		for (Category cat : navigation.getCategories()) {
+//			rv.add(new ExternalLink(rv.newChildId(), "#" + cat.getName(), cat.getName()));
+//		}
 	}
 
 	public Header(IModel<?> model) {
--- paxwicketjava/web/src/main/java/com/hendyirawan/oui/paxwicketjava/web/HomePage.java
+++ paxwicketjava/web/src/main/java/com/hendyirawan/oui/paxwicketjava/web/HomePage.java
@@ -2,6 +2,10 @@ package com.hendyirawan.oui.paxwicketjava.web;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.ops4j.pax.wicket.api.PaxWicketBean;
 
 /**
  * @author ceefour
@@ -10,9 +14,19 @@ import org.apache.wicket.markup.html.basic.Label;
 @SuppressWarnings("serial")
 public class HomePage extends WebPage {
 
+	@PaxWicketBean(name="navigation")
+	private Navigation navigation;
+	
 	public HomePage() {
 		add(new Label("pageTitle", "Buy Amazing Fashion"));
 		add(new Header());
+		add(new ListView<Category>("categories", navigation.getCategories()) {
+			@Override
+			protected void populateItem(ListItem<Category> item) {
+				Category category = item.getModelObject();
+				item.add(new ExternalLink("name", "#" + category.getName(), category.getName()));
+			}
+		});
 		add(new Label("message", "Hello world!"));
 		add(new Footer());
 	}
